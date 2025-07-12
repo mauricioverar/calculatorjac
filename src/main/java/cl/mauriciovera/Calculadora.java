1c6e1273595cced302ea36e1d19d622099a588e1
Mauricio <stb539@gmail.com>
2025-07-12 12:38:36 -0400

    hamcrest

--- src/main/java/cl/mauriciovera/Calculadora.java
+++ src/main/java/cl/mauriciovera/Calculadora.java
@@ -20,4 +20,6 @@ public class Calculadora {
     }
     return a / b;
   }
+
+  
 }
--- src/test/java/cl/mauriciovera/CalculadoraTest.java
+++ src/test/java/cl/mauriciovera/CalculadoraTest.java
@@ -1,33 +1,50 @@
 package cl.mauriciovera;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 import org.junit.jupiter.api.Test;
 
 public class CalculadoraTest {
 
   @Test
-  public void testSumar() {
+  void testSumar() {
     Calculadora calculadora = new Calculadora();
     assertEquals(5, calculadora.sumar(2, 3));
   }
 
   @Test
-  public void testRestar() {
+  void testRestar() {
     Calculadora calculadora = new Calculadora();
     assertEquals(2, calculadora.restar(5, 3));
   }
 
   @Test
-  public void testMultiplicar() {
+  void testMultiplicar() {
     Calculadora calculadora = new Calculadora();
     assertEquals(20, calculadora.multiplicar(4, 5));
   }
 
   @Test
-  public void testDividir() {
+  void testDividir() {
     Calculadora calculadora = new Calculadora();
     assertEquals(5, calculadora.dividir(10, 2));
   }
 
+  @Test
+  void testDividirPorCero() {
+    Calculadora calculadora = new Calculadora();
+    try {
+      calculadora.dividir(10, 0);
+    } catch (IllegalArgumentException e) {
+      assertEquals("No se puede dividir por cero", e.getMessage());
+    }
+  }
+
+  @Test
+  void testSumarConHamcrest() {
+    Calculadora calculadora = new Calculadora();
+    assertThat(calculadora.sumar(2, 3), is(equalTo(5)));
+  }
 }
